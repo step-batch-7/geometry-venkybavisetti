138dacc16156f698029eaac5a4d3a0dcbeda076d
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2019-12-17 17:48:23 +0530

    refactored rectangle class

--- src/rectangle.js
+++ src/rectangle.js
@@ -4,16 +4,16 @@ const getAreaOfTriangle = function(pointA, pointB, pointC) {
   const [x1, y1] = [pointA.x, pointA.y];
   const [x2, y2] = [pointB.x, pointB.y];
   const [x3, y3] = [pointC.x, pointC.y];
-  return Math.abs((1 / 2) * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)));
+  return 0.5 * Math.abs((y2 - y3) * (x1 - x2) - (y1 - y2) * (x2 - x3));
 };
 
 class Rectangle {
   constructor(diagonalEndA, diagonalEndB) {
-    this.AB = new Line(diagonalEndA, {
+    this.side1 = new Line(diagonalEndA, {
       x: diagonalEndB.x,
       y: diagonalEndA.y
     });
-    this.CD = new Line(
+    this.side3 = new Line(
       {
         x: diagonalEndA.x,
         y: diagonalEndB.y
@@ -21,46 +21,52 @@ class Rectangle {
       diagonalEndB
     );
   }
+
   toString() {
-    const diagonalPointA = `(${this.AB.endA.x},${this.AB.endA.y})`;
-    const diagonalPointB = `(${this.CD.endB.x},${this.CD.endB.y})`;
+    const diagonalPointA = `(${this.side1.endA.x},${this.side1.endA.y})`;
+    const diagonalPointB = `(${this.side3.endB.x},${this.side3.endB.y})`;
     return `[Rectangle ${diagonalPointA} to ${diagonalPointB}]`;
   }
+
   get area() {
-    const length = this.AB.length;
-    const breadth = this.CD.endA.findDistanceTo(this.AB.endA);
+    const length = this.side1.length;
+    const breadth = this.side3.endA.findDistanceTo(this.side1.endA);
     return length * breadth;
   }
+
   get perimeter() {
-    const length = this.AB.length;
-    const breadth = this.CD.endA.findDistanceTo(this.AB.endA);
+    const length = this.side1.length;
+    const breadth = this.side3.endA.findDistanceTo(this.side1.endA);
     return 2 * (length + breadth);
   }
+
   isEqualTo(otherRectangle) {
     return (
-      (this.AB.isEqualTo(otherRectangle.AB) &&
-        this.CD.isEqualTo(otherRectangle.CD)) ||
-      (this.AB.isEqualTo(otherRectangle.CD) &&
-        this.CD.isEqualTo(otherRectangle.AB))
+      (this.side1.isEqualTo(otherRectangle.side1) &&
+        this.side3.isEqualTo(otherRectangle.side3)) ||
+      (this.side1.isEqualTo(otherRectangle.side3) &&
+        this.side3.isEqualTo(otherRectangle.side1))
     );
   }
+
   hasPoint(point) {
-    const BC = new Line(this.AB.endB, this.CD.endB);
-    const DA = new Line(this.AB.endA, this.CD.endA);
+    const side2 = new Line(this.side1.endB, this.side3.endB);
+    const side4 = new Line(this.side1.endA, this.side3.endA);
     return (
-      this.AB.hasPoint(point) ||
-      this.CD.hasPoint(point) ||
-      BC.hasPoint(point) ||
-      DA.hasPoint(point)
+      this.side1.hasPoint(point) ||
+      this.side3.hasPoint(point) ||
+      side2.hasPoint(point) ||
+      side4.hasPoint(point)
     );
   }
+
   covers(point) {
     const areaOfRectangle = this.area;
     const areaOfTriangles =
-      getAreaOfTriangle(this.AB.endA, this.AB.endB, point) +
-      getAreaOfTriangle(this.AB.endB, this.CD.endB, point) +
-      getAreaOfTriangle(this.CD.endB, this.CD.endA, point) +
-      getAreaOfTriangle(this.AB.endA, this.CD.endA, point);
+      getAreaOfTriangle(this.side1.endA, this.side1.endB, point) +
+      getAreaOfTriangle(this.side1.endB, this.side3.endB, point) +
+      getAreaOfTriangle(this.side3.endB, this.side3.endA, point) +
+      getAreaOfTriangle(this.side1.endA, this.side3.endA, point);
     return areaOfRectangle == areaOfTriangles;
   }
 }
